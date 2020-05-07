4b29600033429bcb37ae8a70e5b0ba3057d550ae
Yura Kochut <kochutyurawork@gmail.com>
2020-05-07 21:18:18 +0300

    update ( style )

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ class Animation {
     slider() {
         const arrowLeft = document.querySelector('.arrow-left img');
     }
-
 }
 
 
@@ -34,14 +33,24 @@ function fixedNav() {
     const scroll = window.pageYOffset;
     const navbar = document.querySelector('.nav');
     const container = document.querySelectorAll('.container');
+    const list = document.querySelector('.list');
     if (scroll > 60) {
-        navbar.classList.add('nav-fixed')
-        for (const iterator of container) {
-
+        navbar.classList.add('nav-fixed');
+        list.setAttribute('style', 'align-items: center;')
+        for (let i = 0; i < container.length; i++) {
+            console.log(container[i]);
+            if (i !== 0) container[i].setAttribute('style', 'padding-top: 93px;')
+            
         }
 
     } else {
-        navbar.classList.remove('nav-fixed')
+        navbar.classList.remove('nav-fixed');
+        list.setAttribute('style', 'top: -9px;')
+        for (let i = 0; i < container.length; i++) {
+            console.log(container[i]);
+            if (i !== 0) container[i].setAttribute('style', 'padding-top: 0;')
+
+        }
     }
 }
 
@@ -72,7 +81,7 @@ class Slider {
                     break;
                 }
             }
-
+            
             if (nextSlide) { // next slide
                 if (sliderRadio.length > i) { // check nextSlide
                     sliderRadio[i].children[0].checked = true;
@@ -87,7 +96,9 @@ class Slider {
                 }
                 sliderRadio[i].children[0].checked = true;
             }
-
+            console.log(1);
+            
+            
             return i
         }
     }
@@ -99,14 +110,17 @@ class Slider {
 
         switch (radioIndex) {
             case 0:
-                title.textContent = 'Slide 1'
+                title.textContent = 'Slide 1';
+                subTitle.textContent = 'Subtitle of Slider 1'
                 break;
 
             case 1:
                 title.textContent = 'Mileage Made Simple';
+                subTitle.textContent = 'otivation And Your Personal Vision An Unbeatable Force'
                 break;
             case 2:
                 title.textContent = 'Slide 3'
+                subTitle.textContent = 'Subtitle of Slider 3'
                 break;
         }
 
